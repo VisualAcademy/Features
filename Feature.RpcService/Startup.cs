615eaccaaf83625ac5aa4de5f313575f073c7954
RedPlus <redplus@redplus.net>
2019-06-24 09:45:42 +0900

    코드 정리

--- Feature.RpcService/Startup.cs
+++ Feature.RpcService/Startup.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Feature.Models;
+﻿using Feature.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
@@ -45,7 +41,6 @@ namespace Feature.RpcService
             {
                 // Communication with gRPC endpoints must be made through a gRPC client.
                 // To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909
-                endpoints.MapGrpcService<GreeterService>();
                 endpoints.MapGrpcService<FeatureService>();
             });
         }
